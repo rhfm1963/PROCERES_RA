6ed2f2d25fe7192c0cb97b8be98db6adcbef74b7
rhfm1963 <rhfm1963@gmail.com>
2025-06-12 00:52:06 -0400

    Update app.js
    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -42,7 +42,7 @@ const startServer = async () => {
     );
 
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
     });
   } catch (err) {
