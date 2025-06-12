c5d0e48c58324772cdfe6276619a28133217fd65
rhfm1963 <rhfm1963@gmail.com>
2025-06-12 00:45:11 -0400

    Update Main 3

--- backend/src/versiones/config/auth_1.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'clave_secreta_para_desarrollo',
-  jwtExpiration: '1h',
-  refreshTokenExpiration: '7d'
-};
